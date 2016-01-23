eebd809770cac4e201fcdf74e35acb0caad51538
ianmiell <ian.miell@gmail.com>
2016-01-23 22:17:09 +0000

    create

--- jenkinsjob/jenkins_node.py
+++ jenkinsjob/jenkins_node.py
@@ -1,5 +1,9 @@
 import jenkins
 j = jenkins.Jenkins('http://jenkins:8080')
-# TODO: only if it doesn't exist
-j.create_node('jenkinsslave1')
+create = True
+for node in j.get_nodes():
+	if node['name'] == 'jenkinsslave1':
+		create = False
+if create:
+	j.create_node('jenkinsslave1')
 
