794e35404f21408a019913b7fdd1f92fee3d588d
ianmiell <ian.miell@gmail.com>
2016-01-26 08:13:27 +0000

    docker plugins

--- jenkinssetup/scripts/jenkins_plugins.py
+++ jenkinssetup/scripts/jenkins_plugins.py
@@ -4,13 +4,18 @@ import jenkins
 
 j = jenkins.Jenkins('http://jenkins:8080')
 print 'Installing plugins starting'
-info = j.install_plugin("docker-plugin")
-info = j.install_plugin("git")
-info = j.install_plugin("github")
-info = j.install_plugin("urltrigger")
-info = j.install_plugin("openshift-pipeline")
-info = j.install_plugin("openshift-deployer")
-print 'Installing plugins done'
-
-
+j.install_plugin("docker-build-publish")
+j.install_plugin("docker-build-step")
+j.install_plugin("docker-commons")
+j.install_plugin("docker-custom-build-environment")
+j.install_plugin("docker-traceability")
+j.install_plugin("docker-workflow")
+j.install_plugin("docker-notification")
+j.install_plugin("docker-plugin")
+j.install_plugin("git")
+j.install_plugin("github")
+j.install_plugin("urltrigger")
+j.install_plugin("openshift-pipeline")
+j.install_plugin("openshift-deployer")
+print 'Installing plugins done, now restart'
 
