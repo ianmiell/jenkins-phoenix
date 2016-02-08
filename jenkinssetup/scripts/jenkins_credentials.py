9278c6ca1d344d43a45dabc1e1b7081d64473185
ianmiell <ian.miell@gmail.com>
2016-02-08 17:05:01 +0000

    whitespace

--- jenkinssetup/scripts/jenkins_credentials.py
+++ jenkinssetup/scripts/jenkins_credentials.py
@@ -6,9 +6,9 @@ api = Jenkins('http://jenkins:8080')
 # Get a list of all global credentials
 credentials = api.credentials
 for key in credentials.credentials.keys():
-        creds_dict = credentials.credentials[key].get_attributes()
-        if creds_dict['json']['credentials']['description'] == 'jenkinsslave1 credentials':
-                credentialsId = creds_dict['json']['credentials']['id']
+		creds_dict = credentials.credentials[key].get_attributes()
+		if creds_dict['json']['credentials']['description'] == 'jenkinsslave1 credentials':
+				credentialsId = creds_dict['json']['credentials']['id']
 				s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 				s.connect(('jenkins',9001))
 				s.send('CREDENTIALSID ' + credentialsId)
