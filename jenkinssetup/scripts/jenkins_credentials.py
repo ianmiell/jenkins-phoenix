931158b183a43086aa02fe87f15f4b072ca8771d
ianmiell <ian.miell@gmail.com>
2016-02-08 16:32:35 +0000

    desc

--- jenkinssetup/scripts/jenkins_credentials.py
+++ jenkinssetup/scripts/jenkins_credentials.py
@@ -1,35 +1,14 @@
 from jenkinsapi.jenkins import Jenkins
 from jenkinsapi.credential import UsernamePasswordCredential, SSHKeyCredential
-
-#    class UsernamePasswordCredential(Credential)
-#     |  Username and password credential
-#     |
-#     |  Constructor expects following dict:
-#     |      {
-#     |          'credential_id': str,   Automatically set by jenkinsapi
-#     |          'displayName': str,     Automatically set by Jenkins
-#     |          'fullName': str,        Automatically set by Jenkins
-#     |          'typeName': str,        Automatically set by Jenkins
-#     |          'description': str,
-#     |          'userName': str,
-#     |          'password': str
-#     |      }
-
+import socket
 
 api = Jenkins('http://jenkins:8080')
 # Get a list of all global credentials
-#TODO
 credentials = api.credentials
-#for credential in credentials:
-#	for key in credential.credentials.keys():
-#		print credential.credentials[key]
-#			#if credential.credentials['description'] == 'jenkinsslave1 '
-#creds = api.credentials
-credentialsId = credentials.credentials.keys()[0]
-
-import socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('jenkins',9001))
-s.send('CREDENTIALSID ' + credentialsId)
-
-
+for key in credentials.credentials.keys():
+        creds_dict = credentials.credentials[key].get_attributes()
+        if creds_dict['json']['credentials']['description'] == 'jenkinsslave1 credentials':
+                credentialsId = creds_dict['json']['credentials']['id']
+				s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+				s.connect(('jenkins',9001))
+				s.send('CREDENTIALSID ' + credentialsId)
