6b8e8326725ceb4450a760b63b1fc6f01038c8b3
ianmiell <ian.miell@gmail.com>
2016-02-20 14:50:00 +0000

    add a wait

--- jenkinsswarmslave1/startup.sh
+++ jenkinsswarmslave1/startup.sh
@@ -3,6 +3,8 @@ SLAVE_JENKINS_SERVER=jenkins
 SLAVE_JENKINS_PORT=8080
 SLAVE_JENKINS_LABELS=swarm
 SLAVE_JENKINS_HOME=${JENKINS_HOME:-$HOME}
+echo "Wait three minutes before starting swarm client"
+sleep 180
 echo "Starting up swarm client with args:"
 echo "$@"
 echo "and env:"
